1c02a05107cfb1521271a107c8b2b3e8fdd6aafd
Riya Patro <riyapatro18@gmail.com>
2023-07-09 19:46:08 +0530

    my movix website code is completed

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useState,useEffect } from "react";
+import { BrowserRouter,Routes,Route  } from "react-router-dom";
+import {fetchDataFromApi} from "./utils/api.js"
+import { useSelector, useDispatch } from 'react-redux'
+import { getApiConfiguration } from "./store/homeSlice.js";
+
+
+// pages
+import Header from "./components/header/Header.jsx"
+import Footer from "./components/footer/Footer.jsx"
+import PageNotFound from "./pages/404/PageNotFound.jsx"
+import Details from "./pages/details/Details.jsx"
+import Explore from "./pages/explore/Explore.jsx"
+import Home from "./pages/home/Home.jsx"
+import SearchResult from "./pages/searchResult/SearchResult.jsx"
+
+
+function App() {
+  const dispatch = useDispatch()
+  const {url} = useSelector((state)=>
+    state.home
+  )
+  console.log(url)
+
+   useEffect(()=>{
+   fetchApiConfig()
+   },[])
+
+  const fetchApiConfig=()=>{
+    fetchDataFromApi("/configuration")
+    .then((res)=>{
+      console.log(res)
+      
+      const url ={
+        backdrop:res.images.secure_base_url + "original",
+        poster:res.images.secure_base_url + "original",
+        profile:res.images.secure_base_url + "original",
+
+      }
+
+      dispatch(getApiConfiguration(url))
+    })
+  }
+  return ( 
+  <>
+ <BrowserRouter>
+ <Header/>
+ <Routes>
+  <Route path="/" element={<Home/>}/>
+  <Route path="/:mediaType/:id" element={<Details/>}/>
+  <Route path="/search/:query" element={<SearchResult/>} />
+  <Route path="/explore/:mediaType" element={<Explore/>} />
+  <Route path="*" element={<PageNotFound/>}  />
+ </Routes>
+ <Footer/>
+ </BrowserRouter>
+  </>
+  )
+}
+
+export default App;
